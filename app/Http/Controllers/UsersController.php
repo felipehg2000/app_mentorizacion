91241a6dca1718ee237929408e2238e71dc939df
Felipe Hernández González <FelipeHG2000>
2023-04-18 21:09:34 +0200

    UserController: modificado login.
    Feat: se modifica el login para que en caso de que sea correcto se
    redirija de forma correcta.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\MentorsController;
 
 
 
@@ -43,20 +45,22 @@ class UsersController extends Controller
         $clave_cifrada = self::cifrate_private_key($request->password);
         $user          = User::where('user', $request->user)->where('password', $clave_cifrada)->first();
 
-        if ($user != NULL)
-            {
+        if ($user != NULL) {
             Auth::login($user);
-            if ($user->user_type == 1)
-                {
-                return view('students.index');
-                }
-            else
-                {
-                return view('mentors.index');
-                }
+            if ($user->user_type == 1){
+                $controlador_estudiante = new StudentsController();
+                $vista_estudiante = $controlador_estudiante->index();
+                return $vista_estudiante;
+
             }
-        else
-        {
+            else{
+                $controlador_mentores = new MentorsController();
+                $vista_mentor = $controlador_mentores->index();
+
+                return $vista_mentor;
+            }
+        }
+        else{
         return redirect()->back()->withErrors(['message' => 'El correo electrónico o la contraseña son incorrectos.']);
         }
     }
@@ -94,10 +98,6 @@ class UsersController extends Controller
     /**
      * Modificar datos usuario
      * =======================
-<<<<<<< HEAD
-
-=======
->>>>>>> 79abd68237831c9eaac9f08426294c1ec89c4fba
      * Función modify carga los datos en una variable y muestra la vista correspondiente con los datos introducidos para que el usuario pueda verlos
      * y modificarlos.
      * En la función store se cargan los datos en la tupla adecuada y se hace el update de estos para modificarlo
@@ -147,11 +147,7 @@ class UsersController extends Controller
      * no pueda modificar los datos.
      * Función delete_store se borran los datos y se redirige a la ventana de home.
      *
-<<<<<<< HEAD
      * TO DO:
-=======
-     * To Do:
->>>>>>> 79abd68237831c9eaac9f08426294c1ec89c4fba
      * ======
      * Pedir la contraseña antes de borrar los datos, para asegurarnos de que realmente quieren borrar los datos.
      * Borrar la caché de la pagina web para que al darle atrás no vaya a ninguna página.
