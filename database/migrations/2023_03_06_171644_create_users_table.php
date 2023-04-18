c67867e055c6a9bf7847093c7cdbb83652651195
Felipe Hernández González <FelipeHG2000>
2023-04-18 21:04:09 +0200

    Migration: modificada
    Feat: se modifica la migración para añadir las tablas de herencia
    de estudiantes y mentores y la tabla que relaciona las herencias.
    
    Feat: Se crea función para pasar los datos de una imagen a binario

--- database/migrations/2023_03_06_171644_create_users_table.php
+++ database/migrations/2023_03_06_171644_create_users_table.php
@@ -11,35 +11,85 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration{
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void{
+        /**
+         * TABLA USERS:
+         * ============
+         *
+         * Tabla que guarda los datos más genéricos de los usuarios
+         */
         Schema::create('users', function (Blueprint $table) {
             $table->id           (                 );
             $table->string       ('name'       , 30);
             $table->string       ('surname'    , 90)->nullable();
             $table->string       ('email'          )->unique  ();
+            $table->timestamp    ('email_verified_at')->nullable();
             $table->string       ('user'       , 30)->unique  ();
             $table->text         ('password'       );
             $table->boolean      ('user_type'      );
             $table->integer      ('study_area'     );
             $table->text         ('description'    )->nullable();
+            $table->binary       ('image'          )->default($this->imageToBinary('photos/default_user_image.png'));
             $table->rememberToken(                 );//en caso de que el usuario decida tener la sesión abierta se guardará un token
             $table->timestamps   (                 );
+        });
+
+        /**
+         * TABLA STUDENTS
+         * ==============
+         *
+         * Datos específicos de los usuarios de tipo estudiante. Herenciada de la tabla usuarios, tienen la misma clave.
+         */
+        Schema::create('students', function (Blueprint $table){
+            $table->integer('user_id')->primary();
+        });
+
+        /**
+         * TABLA MENTROS
+         * =============
+         *
+         * Datos específicos de los usuarios de tipo mentor. Herencia de la tabla usuarios, tienen la misma clave.
+         */
+        Schema::create('mentors', function (Blueprint $table){
+            $table->integer('user_id')->primary();
+        });
+
+        /**
+         * TABLA INHERITANCE_USERS:
+         * ========================
+         *
+         * Herencia de los usuarios almacenará la clave del usuario y el nombre de la tabla a la que habrá que acceder para ver el resto de datos.
+         */
+        Schema::create('inheritance_users', function (Blueprint $table){
+            $table->integer('user_id')   ->primary();
+            $table->string ('table_name');
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::dropIfExists('users');
+    public function down(): void{
+        Schema::dropIfExists('users'            );
+        Schema::dropIfExists('students'         );
+        Schema::dropIfExists('mentors'          );
+        Schema::dropIfExists('inheritance_users');
     }
+
+    /**
+     * Función para subir los datos de las imagenes en binario
+     */
+    private function imageToBinary(String $path){
+        $image_path = public_path($path);
+        $image_data = file_get_contents($image_path);
+        return base64_encode($image_data);
+    }
+
 };
