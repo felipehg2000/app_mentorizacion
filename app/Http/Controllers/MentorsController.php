1ad31c47bb4867a17d14a334f1ed04c953d76277
Felipe Hernández González <FelipeHG2000>
2023-06-06 23:57:42 +0200

    Feat: funcionalidad par a aceptar solicitudes de amistad.
    Feat: se crea la vista para la aceptación o denegación de peticiones
    de amistad
    Feat: se implementan en el controlador las funciones anteriormente
    creadas para visualizar a los estudiantes que nos han mandado solicitudes
    de amistar y poder aceptar o denegar sus solicitudes

--- app/Http/Controllers/MentorsController.php
+++ app/Http/Controllers/MentorsController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\Student;
 use App\Models\User;
 
 class MentorsController extends Controller{
@@ -28,12 +30,37 @@ class MentorsController extends Controller{
      * Función post: Se guarda en la base de datos que el estado es aceptado o se borra el registro en caso de que sea denegada la solicitud
      */
     public function friendship(){
-        //TO DO: tengo que hacer la consulta select para pasarle la variable users con los datos de los usuarios que hayan seguido al mentor y en la vista modificar el botón actual por dos botones.
-        //Pasar para probar los datos del estudiante también, osea los datos del usuario y el estudiante
-        return view('mentors.friendship');
+        $result_user = User::join('FRIEND_REQUESTS', 'FRIEND_REQUESTS.STUDENT_ID', '=', 'USERS.id')
+                           ->select('users.*')
+                           ->get();
+        /*$result_student = Student::where('id', $result_user->id)
+                                 ->get();
+*/
+        foreach($result_user as $user){
+            $this->convertToPhoto($user->IMAGE, $user->USER);
+        }
+
+        return view('mentors.friendship', compact('result_user'));
     }
     public function friendship_store (Request $request){
+        $mentor_id  = Auth::user()->id;
+        $student_id = DB::table('USERS')->select('ID')->where('user', $request->user_user)->get()->first()->ID;
+        if ($request->respuesta == "ACEPTAR"){
+            //Modificar el estado de la solicitud de amistad
+            echo 'ACEPTADA';
+            DB::table('FRIEND_REQUESTS')
+              ->where('STUDENT_ID', $student_id)
+              ->where('MENTOR_ID', $mentor_id)
+              ->update(['STATUS' => 2]);
 
+        }else if ($request->respuesta == "DENEGAR"){
+            //Borrar la solicitud de amistad
+            echo 'DENEGADA';
+            DB::table('FRIEND_REQUESTS')
+              ->where('STUDENT_ID', '=', $student_id)
+              ->where('MENTOR_ID', $mentor_id)
+              ->delete();
+        }
     }
     //--------------------------------------------------------------------------------------------------
     /**
@@ -48,4 +75,20 @@ class MentorsController extends Controller{
         file_put_contents($destination_path, $image_data);
     }
     //--------------------------------------------------------------------------------------------------
+    private function convertToPhoto($blopPhoto, $user){
+        //Creamos la ruta y el archivo
+        $path             = "photos/users/User" . $user. ".png";
+        $destination_path = public_path($path);
+
+        touch($destination_path);
+
+        //Convertimos el campo blop en una imagen
+        $image_binary = $blopPhoto;
+        $image_data   = base64_decode($image_binary);
+
+        //Guardamos los datos de la imagen en el file creado
+        if (file_exists($destination_path)){
+            file_put_contents($destination_path, $image_data);
+        }
+    }
 }
--- resources/views/mentors/friendship.blade.php
+++ resources/views/mentors/friendship.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.plantillaUsuLogeado')
 
-@section('title', 'Student')
+@section('title', 'Mentores')
 
 @section ('style')
     <link href="{{ asset('css/friendshipStyle.css') }}" rel="stylesheet">
@@ -8,7 +8,7 @@
 
 @section ('main')
     <div class="pnlPrincipalFriendship">
-        @foreach ($users as $user)
+        @foreach ($result_user as $user)
             <div class="pnlCard">
                 <div class="pnlSuperiorCard">
                     <img src=" {{ asset('photos/users/User' . $user->USER . '.png')}}">
@@ -17,15 +17,17 @@
                 <div class="pnlInferiorCard">
                     <div class="name">{{ $user->NAME }} {{ $user->SURNAME}}</div><br>
                     <label>{{ $user->DESCRIPTION }}</label><br>
-                    <form action="{{ route('students.friendship.store')}}"  method="POST">
+                    <form action="{{ route('mentors.friendship.store')}}"  method="POST">
                         @csrf
+                        <input class="ocultar", id="respuesta" name="respuesta" type="hidden" value="Patata">
                         <input class="ocultar", id="user_user" name="user_user" value = {{$user->USER}} type="hidden">
-
                         @if ($errors->first('error') == $user->USER)
                             <br><small>Solicitud de amsitad enviada anteriormente.</small><br>
                         @endif
                         <div class="btnFollow">
-                            <button type="submit" class="btnFollow" id="btnFollow">Unirme</button>
+                            <button type="submit" class="btnFollow" id="btnFollow" onmouseover="cambiarInputOculto('ACEPTAR')">Aceptar</button>
+                            <button type="submit" class="btnFollow" id="btnFollow" onmouseover="cambiarInputOculto('DENEGAR')">Denegar</button>
+
                         </div>
                     </form>
                 </div>
@@ -34,4 +36,11 @@
     </div>
 
     </main>
+
+    <script>
+        function cambiarInputOculto(variable){
+            console.log(1);
+            document.getElementById("respuesta").value = variable;
+        }
+    </script>
 @endsection
