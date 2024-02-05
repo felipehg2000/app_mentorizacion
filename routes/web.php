53a3b983cc46f5c5f3b4ac6d4a2ab457ebfe96f2
Felipe Hernández González <FelipeHG2000>
2024-02-05 12:02:06 +0100

    Fix: routes: una ruta estaba mal escrita, la arreglamos

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\StudentsController;
  * @Email: felipehg2000@usal.es
  * @Date: 2023-03-06 23:03:30
  * @Last Modified by: Felipe Hernández González
- * @Last Modified time: 2024-02-01 19:39:16
+ * @Last Modified time: 2024-02-05 11:52:24
  * @Description: De este archivo se leerán las rutas a las que el usuario pueda acceder, es decir, el usuario solo puede acceder a las rutas que especifiquemos aquí.
  *               Como buena práctica hay que darle nombre a cada una de las rutas, para que si una de estas cambie no haya que cambiar todos los lugares donde las referenciemos,
  *               para esto usaremos la función name.
@@ -29,7 +29,7 @@ Route::controller(UsersController::class)->group(function(){
     Route::get  ('users'                , [UsersController::class, 'index'                  ])->name('users.index'                  );
     Route::post ('users'                , [UsersController::class, 'store'                  ])->name('users.store'                  );
     Route::get  ('users/sync_chat'      , [UsersController::class, 'sync_chat'              ])->name('users.sync_chat'              );
-    Route::post ('user/sync_chat'       , [UsersController::class, 'sync_chat_store'        ])->name('sync_chat.store'              );
+    Route::post ('users/sync_chat'      , [UsersController::class, 'sync_chat_store'        ])->name('users.sync_chat.store'        );
     Route::get  ('users/friendship'     , [UsersController::class, 'friendship'             ])->name('users.friendship'             );
     Route::get  ('users/actual_friends' , [UsersController::class, 'actual_friends'         ])->name('users.actual_friends'         );
     Route::get  ('users/create'         , [UsersController::class, 'create'                 ])->name('users.create'                 );
