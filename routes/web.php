d1fa3c5c399390e1e5b855ef787bbf8b7098e4b6
Felipe Hernández González <FelipeHG2000>
2024-02-23 21:21:46 +0100

    Feat: rutas introducidas en la web

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\StudentsController;
  * @Email: felipehg2000@usal.es
  * @Date: 2023-03-06 23:03:30
  * @Last Modified by: Felipe Hernández González
- * @Last Modified time: 2024-02-06 10:12:47
+ * @Last Modified time: 2024-02-23 21:02:18
  * @Description: De este archivo se leerán las rutas a las que el usuario pueda acceder, es decir, el usuario solo puede acceder a las rutas que especifiquemos aquí.
  *               Como buena práctica hay que darle nombre a cada una de las rutas, para que si una de estas cambie no haya que cambiar todos los lugares donde las referenciemos,
  *               para esto usaremos la función name.
@@ -28,6 +28,9 @@ Route::get('/', HomeController::class)->name('home');
 Route::controller(UsersController::class)->group(function(){
     Route::get  ('users'                , [UsersController::class, 'index'                  ])->name('users.index'                  );
     Route::post ('users'                , [UsersController::class, 'store'                  ])->name('users.store'                  );
+    Route::get  ('users/task_board'     , [UsersController::class, 'task_board'             ])->name('users.task_board'             );
+    Route::post ('users/task_board'     , [UsersController::class, 'task_board_store'       ])->name('users.task_board.store'       );
+    Route::post ('users/add_task'       , [UsersController::class, 'add_task_store'         ])->name('users.add_task.store'         );
     Route::get  ('users/sync_chat'      , [UsersController::class, 'sync_chat'              ])->name('users.sync_chat'              );
     Route::post ('users/sync_chat'      , [UsersController::class, 'sync_chat_store'        ])->name('users.sync_chat.store'        );
     Route::post ('users/send_message'   , [UsersController::class, 'send_message_store'     ])->name('users.send_message.store'     );
