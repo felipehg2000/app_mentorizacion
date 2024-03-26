c2aa4335ecd90b517a8ff2c885d3b70a1f5109ce
Felipe Hernández González <FelipeHG2000>
2024-03-26 10:04:52 +0100

    Fix unistall vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,2 @@
 import './bootstrap';
 import 'laravel-datatables-vite';
-import 'vue';
-
-/*const app = new Vue({
-    el: '#app',
-    mounted(){
-        window.Echo.channel('sync_chat')
-        .listen('NewMessageEvent', (e) => {
-            console.log('Mensaje en tiempo real')
-        });
-    }
-});
-*/
--- resources/views/layouts/plantillaUsuLogeado.blade.php
+++ resources/views/layouts/plantillaUsuLogeado.blade.php
@@ -47,101 +47,99 @@
     @yield('js')
 </head>
 <body>
-    <div id='app'>
-        <!--HEADER-->
-        <!--Parte de la navegación-->
-        <nav>
-            <img src = " {{ asset('photos/logo_blanco.JPG') }}" class="logo">
-            <img src = " {{ asset('photos/my_image.png') }} " class="perfil_image">
-        </nav>
-        <!--Pantalla sin la parte de la navegación-->
-        <div class="pnlMain">
-            <!--Panel princiapl-->
-            <div class="pnlPrincipal">
-                <!--Apartado del menú-->
-                <div class="pnlLeft">
-                    <div class="menu">Tablón de anuncios      </div>
-                        <div class="submenu" id="submenu_1" onclick="redirection(1)">Tablón completo   </div>
-                        <div class="submenu" id="submenu_2" onclick="redirection(2)">Tareas completadas</div>
-                        <div class="submenu" id="submenu_3" onclick="redirection(3)">Tareas a completar</div>
+    <!--HEADER-->
+    <!--Parte de la navegación-->
+    <nav>
+        <img src = " {{ asset('photos/logo_blanco.JPG') }}" class="logo">
+        <img src = " {{ asset('photos/my_image.png') }} " class="perfil_image">
+    </nav>
+    <!--Pantalla sin la parte de la navegación-->
+    <div class="pnlMain">
+        <!--Panel princiapl-->
+        <div class="pnlPrincipal">
+            <!--Apartado del menú-->
+            <div class="pnlLeft">
+                <div class="menu">Tablón de anuncios      </div>
+                    <div class="submenu" id="submenu_1" onclick="redirection(1)">Tablón completo   </div>
+                    <div class="submenu" id="submenu_2" onclick="redirection(2)">Tareas completadas</div>
+                    <div class="submenu" id="submenu_3" onclick="redirection(3)">Tareas a completar</div>
 
-                    <div class="menu">Chats               </div>
-                        <div class="submenu" id="submenu_4" onclick="redirection(4)">Chats privados</div>
+                <div class="menu">Chats               </div>
+                    <div class="submenu" id="submenu_4" onclick="redirection(4)">Chats privados</div>
 
-                    <div class="menu">Tutorías                  </div>
-                        <div class="submenu" id="submenu_6" onclick="redirection(6)">Solicitudes        </div>
-                        <div class="submenu" id="submenu_7" onclick="redirection(7)">Acceso a la tutoría</div>
+                <div class="menu">Tutorías                  </div>
+                    <div class="submenu" id="submenu_6" onclick="redirection(6)">Solicitudes        </div>
+                    <div class="submenu" id="submenu_7" onclick="redirection(7)">Acceso a la tutoría</div>
 
-                    <div class="menu">Sala de estudios         </div>
-                        <div class="submenu" id="submenu_8" onclick="redirection(8)">Integrantes       </div>
-                        <div class="submenu" id="submenu_9" onclick="redirection(9)">Solicitudes    </div>
+                <div class="menu">Sala de estudios         </div>
+                    <div class="submenu" id="submenu_8" onclick="redirection(8)">Integrantes       </div>
+                    <div class="submenu" id="submenu_9" onclick="redirection(9)">Solicitudes    </div>
 
-                    <div class="menu">Guías de uso</div>
-                        <div class="submenu" id="submenu_5"  onclick="redirection(5)">Primeros pasos        </div>
-                        <div class="submenu" id="submenu_13" onclick="redirection(13)">Novedades        </div>
+                <div class="menu">Guías de uso</div>
+                    <div class="submenu" id="submenu_5"  onclick="redirection(5)">Primeros pasos        </div>
+                    <div class="submenu" id="submenu_13" onclick="redirection(13)">Novedades        </div>
 
-                    <div class="menu">Gestión de usuario        </div>
-                        <div class="submenu" id="submenu_10" onclick="redirection(10)">Modificar mis datos </div>
-                        <div class="submenu" id="submenu_11" onclick="redirection(11)">Eliminar mi cuenta  </div>
-                        <div class="submenu" id="submenu_12" onclick="redirection(12)">Cerrar sesión       </div>
-                </div>
-                <!--Apartado del resto de la pantalla-->
-                <div class="pnlRight">
-                    <div class='pnlCarga' id='pnlCarga'>
-                        <i class="fa-li fa fa-spinner fa-spin"></i>
-                    </div>
-
-                    <div class='pnlCubierta' id='pnlCubierta'>
-                        <div class='pnlCubiertaMensaje' id="pnlCubiertaMensaje"></div>
-                    </div>
-
-                    @yield('main')
-                </div>
+                <div class="menu">Gestión de usuario        </div>
+                    <div class="submenu" id="submenu_10" onclick="redirection(10)">Modificar mis datos </div>
+                    <div class="submenu" id="submenu_11" onclick="redirection(11)">Eliminar mi cuenta  </div>
+                    <div class="submenu" id="submenu_12" onclick="redirection(12)">Cerrar sesión       </div>
             </div>
-            <!--Panel para oscurecer la pantalla-->
-            <div class="pnlOscurecer" id="pnlOscurecer">
-            </div>
-            <!--Ventana emergente-->
-            <div class="pnlEmergente" id="pnlEmergente">
-                <div class="pnlEmergenteTitulo">
-                    <i class="fa fa-exclamation-triangle" style="font-size:24px;color:white"></i>
-                    <p class="tituloEmergente"> Aviso </p>
+            <!--Apartado del resto de la pantalla-->
+            <div class="pnlRight">
+                <div class='pnlCarga' id='pnlCarga'>
+                    <i class="fa-li fa fa-spinner fa-spin"></i>
                 </div>
-                <div class="pnlEmergentePrincipal">
-                    <p class="textoEmergente" id="textoEmergente"> Texto emergente</p>
-                    <br>
-                    <input class="edtPnlEmergente" id="edtPnlEmergente" type="password" placeholder="Introduzca su contraseña">
-                </div>
-                <div class="pnlEmergenteBotones">
-                    @csrf
-                    <button class="btnEmergente" id="btnEmergenteAceptar" type="submit" onclick="aceptarEmergente()">
-                        Aceptar
-                    </button>
-                    <button class="btnEmergente" id="btnEmergenteCancelar" type="submit" onclick="cerrarEmergetne()">
-                        Cancelar
-                    </button>
-                </div>
-            </div>
 
-            <div class='pnlRespuestaEmergente' id='pnlRespuestaEmergente'>
-                <div class='pnlEmergenteTitulo'>
-                    <i class="fa fa-exclamation-triangle" style="font-size:24px;color:white"></i>
-                    <p class="tituloEmergente"> Aviso </p>
+                <div class='pnlCubierta' id='pnlCubierta'>
+                    <div class='pnlCubiertaMensaje' id="pnlCubiertaMensaje"></div>
                 </div>
-                <div class='pnlEmergentePrincipal'>
-                    <p class='textoEmergente' id='textoEmergenteRespuesta'>Texto respuesta emergente</p>
-                </div>
-                <div class='pnlEmergenteBotones'>
-                    <button class="btnEmergente" id="btnEmergenteAceptar" type="submit" onclick="aceptarPnlRespuestaEmergente()">
-                        Aceptar
-                    </button>
-                </div>
-            </div>
 
-            @yield('panelesEmergentes')
+                @yield('main')
+            </div>
+        </div>
+        <!--Panel para oscurecer la pantalla-->
+        <div class="pnlOscurecer" id="pnlOscurecer">
+        </div>
+        <!--Ventana emergente-->
+        <div class="pnlEmergente" id="pnlEmergente">
+            <div class="pnlEmergenteTitulo">
+                <i class="fa fa-exclamation-triangle" style="font-size:24px;color:white"></i>
+                <p class="tituloEmergente"> Aviso </p>
+            </div>
+            <div class="pnlEmergentePrincipal">
+                <p class="textoEmergente" id="textoEmergente"> Texto emergente</p>
+                <br>
+                <input class="edtPnlEmergente" id="edtPnlEmergente" type="password" placeholder="Introduzca su contraseña">
+            </div>
+            <div class="pnlEmergenteBotones">
+                @csrf
+                <button class="btnEmergente" id="btnEmergenteAceptar" type="submit" onclick="aceptarEmergente()">
+                    Aceptar
+                </button>
+                <button class="btnEmergente" id="btnEmergenteCancelar" type="submit" onclick="cerrarEmergetne()">
+                    Cancelar
+                </button>
+            </div>
         </div>
 
-        @stack('scripts_yajra')
+        <div class='pnlRespuestaEmergente' id='pnlRespuestaEmergente'>
+            <div class='pnlEmergenteTitulo'>
+                <i class="fa fa-exclamation-triangle" style="font-size:24px;color:white"></i>
+                <p class="tituloEmergente"> Aviso </p>
+            </div>
+            <div class='pnlEmergentePrincipal'>
+                <p class='textoEmergente' id='textoEmergenteRespuesta'>Texto respuesta emergente</p>
+            </div>
+            <div class='pnlEmergenteBotones'>
+                <button class="btnEmergente" id="btnEmergenteAceptar" type="submit" onclick="aceptarPnlRespuestaEmergente()">
+                    Aceptar
+                </button>
+            </div>
+        </div>
+
+        @yield('panelesEmergentes')
     </div>
+
+    @stack('scripts_yajra')
 </body>
 </html>
