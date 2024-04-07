1a9fb4ceb74d54fa547ec77e996a804a93f82c93
Felipe Hernández González <FelipeHG2000>
2024-04-07 23:40:22 +0200

    Feat: creamos todas las vistas para el administrador

---
+++ resources/views/admins/delete.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.plantillaAdminLoegeado')
+
+@section ('style')
+    <link href="{{ asset('css/done_tasksStyle.css')  }}" rel="stylesheet">
+@endsection
+
+@section('js')
+    <script src="{{ asset('js/User/done_tasksScript.js') }}"></script>
+
+@endsection
+
+@section ('main')
+    <p>Hola</p>
+
+@endsection
