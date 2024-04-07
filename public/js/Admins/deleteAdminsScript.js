ecab2c6337336364837d08a58de05b1733b67840
Felipe Hernández González <FelipeHG2000>
2024-04-08 00:38:26 +0200

    Feat: implementamos la vista con su js para delete admin

--- public/js/Admins/deleteAdminsScript.js
+++ public/js/Admins/deleteAdminsScript.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
     let dataTable = new DataTable('data_table_name');
 });
+
+function AdminClickTable(param_user_id){
+    var data = {
+        _token : csrfToken,
+        id     : param_user_id
+    }
+
+    $.ajax({
+        url   : url_delete_admin,
+        method: 'POST'         ,
+        data  : data
+    }).done(function(respuesta){
+        if(respuesta.success){
+            alert('Usuario borrado correctamente');
+        } else {
+            alert('El usuario no se ha podido borrar. No se ha encontrado o quería eliminar su propia cuenta');
+        }
+
+        $('#data_table_name').DataTable().ajax.reload();
+    });
+}
--- resources/views/admins/delete.blade.php
+++ resources/views/admins/delete.blade.php
@@ -5,11 +5,26 @@
 @endsection
 
 @section('js')
-    <script src="{{ asset('js/User/done_tasksScript.js') }}"></script>
+    <script src="{{ asset('js/Admins/deleteAdminsScript.js') }}"></script>
+
+    <script>
+        var url_delete_admin = "{{ route('admin.delete.store') }}";
+    </script>
 
 @endsection
 
 @section ('main')
-    <p>Hola</p>
-
+    <div class="container">
+        <div class="card">
+            <div class="card-header">Borrar administradores:</div>
+            <div class="card-body">
+                {{ $dataTable->table() }}
+            </div>
+        </div>
+    </div>
 @endsection
+
+@push('scripts_yajra')
+    {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+@endpush
+
