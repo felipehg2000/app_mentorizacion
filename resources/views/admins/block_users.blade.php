638751dac825fcf5f8033834815d04d80c85f6c7
Felipe Hernández González <FelipeHG2000>
2024-04-07 23:41:11 +0200

    Feat: creamos la vista, data table y js para bloquear usuarios, lo implementamos

---
+++ resources/views/admins/block_users.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.plantillaAdminLoegeado')
+
+@section ('style')
+    <link href="{{ asset('css/done_tasksStyle.css')  }}" rel="stylesheet">
+@endsection
+
+@section('js')
+    <script src="{{ asset('js/Admins/block_usersScript.js') }}"></script>
+
+    <script>
+        var url_bann_people = "{{ route('admin.bann_people.store') }}";
+    </script>
+@endsection
+
+@section ('main')
+<p style="visibility: hidden">{{$tipo_usu = 1}}<
+    <div class="container">
+        <div class="card">
+            <div class="card-header">Usuarios:</div>
+            <div class="card-body">
+                {{ $dataTable->table() }}
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts_yajra')
+    {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+@endpush
+
+
+
